84f9524c2bb110618343d72077fe8e5e4995941f
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-06-24 14:16:05 +0200

    Add image to each poster with php

--- milestone-1/index.php
+++ milestone-1/index.php
@@ -17,48 +17,43 @@
 </head>
 
 <body>
-    <div id="app">
-        <header id='site_header'>
-            <div class="logo">
-                <img src="./assets/img/spotify-logo-png-7053.png" alt="">
-            </div>
-            <div>
-                <select name="select" id="select" v-model="selected">
-                    <option value="" disabled selected>Select your option</option>
-                    <?php foreach($genres as $genre){ ?>
-                       <option value=""><?= $genre ?></option> 
-                   <?php } ?>
-                </select>
-            </div>
-        </header>
-        <main id="site_main">
-            <div v-for="album in albums" class="card"
-                v-if="selected === album.genre || selected === 'All' || selected ==='' ">
+    <header id='site_header'>
+        <div class="logo">
+            <img src="./assets/img/spotify-logo-png-7053.png" alt="">
+        </div>
+        <div>
+            <select name="select" id="select" v-model="selected">
+                <option value="" disabled selected>Select your option</option>
+                <?php foreach($genres as $genre){ ?>
+                    <option value=""><?= $genre ?></option> 
+                <?php } ?>
+            </select>
+        </div>
+    </header>
+    <main id="site_main">
+    <?php 
+        foreach($data as $album){ ?>
+            <div class="card">
                 <div class="album_image">
                     <div class="overlay">
                         <i class="fas fa-play"></i>
                     </div>
-                    <img :src="album.poster" alt="">
-                </div>
-                <h2>{{album.title.toUpperCase()}}</h2>
-                <div class="album_info">
-                    <h4>{{album.author}}</h4>
-                    <h4>{{album.year}}</h4>
+                    <img src="<?= $album["poster"] ?>" alt="">
                 </div>
             </div>
-        </main>
+        <?php }
+    ?>
+        
+            <!-- v-if="selected === album.genre || selected === 'All' || selected ==='' "> -->
+            
+            <h2>{{album.title.toUpperCase()}}</h2>
+            <div class="album_info">
+                <h4>{{album.author}}</h4>
+                <h4>{{album.year}}</h4>
+            </div>
+        
+    </main>
 
-
-
-
-    </div>
-
-    <!-- AJAX Axios -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"
-        integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <!-- Vue JS-->
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
     <!-- Main JS file -->
     <script src="./assets/js/main.js"></script>
 </body>
